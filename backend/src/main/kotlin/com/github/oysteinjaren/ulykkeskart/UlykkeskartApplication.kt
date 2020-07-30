b71c7b4e9a262944576e0c3389b6c72df23be587
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-07-30 12:11:58 +0200

    Oppretter backend-prosjekt med Spring Boot, Kotlin og Gradle
    Foreløpig kun to dependencies: spring-boot-starter-web og spring-boot-devtools

---
+++ backend/src/main/kotlin/com/github/oysteinjaren/ulykkeskart/UlykkeskartApplication.kt
@@ -0,0 +1,11 @@
+package com.github.oysteinjaren.ulykkeskart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UlykkeskartApplication
+
+fun main(args: Array<String>) {
+    runApplication<UlykkeskartApplication>(*args)
+}
