40670fc2cd0204b2e125556998ebc16de6d1dd49
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-08-07 10:54:34 +0200

    Splitter ut Ulykker som en egen component

--- web/src/App.js
+++ web/src/App.js
@@ -1,80 +1,17 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
-import useSwr from "swr";
 import "./App.css";
 
-const fetcher = (...args) => fetch(...args).then((response) => response.json());
+import Ulykker from "./components/Ulykker";
 
 function App() {
-  const ulykkerUrl = "/api/ulykker";
-  const { data, error } = useSwr(ulykkerUrl, { fetcher });
-  const ulykker = data && !error ? data : [];
-
-  const [aktivUlykke, setAktivUlykke] = React.useState(null);
-
-  function hentUlykkeIkon(alvorlighetsgrad) {
-    var sti = "";
-    switch (alvorlighetsgrad) {
-      case "USKADET":
-        sti = "/images/marker-icon-green.png";
-        break;
-      case "LETTERESKADET":
-        sti = "/images/marker-icon-yellow.png";
-        break;
-      case "ALVORLIGSKADET":
-        sti = "/images/marker-icon-orange.png";
-        break;
-      case "MEGETALVORLIGSKADET":
-        sti = "/images/marker-icon-red.png";
-        break;
-      case "DREPT":
-        sti = "/images/marker-icon-black.png";
-        break;
-      default:
-        sti = "/images/marker-icon-gray.png";
-        break;
-    }
-    return new Icon({
-      iconUrl: sti,
-      iconSize: [25, 41],
-      iconAnchor: [12, 40],
-    });
-  }
-
   return (
     <Map center={[63.430515, 10.395053]} zoom={12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-
-      {ulykker &&
-        ulykker
-          .filter((ulykke) => ulykke.koordinater != null)
-          .map((ulykke) => (
-            <Marker
-              key={ulykke.id}
-              position={[ulykke.koordinater.lat, ulykke.koordinater.lon]}
-              icon={hentUlykkeIkon(ulykke.alvorlighetsgrad)}
-              onclick={() => {
-                setAktivUlykke(ulykke);
-              }}
-            />
-          ))}
-      {aktivUlykke && (
-        <Popup
-          position={[aktivUlykke.koordinater.lat, aktivUlykke.koordinater.lon]}
-          onClose={() => {
-            setAktivUlykke(null);
-          }}
-        >
-          <div>
-            <h2>{aktivUlykke.ulykkesdato}</h2>
-            <p>{aktivUlykke.alvorlighetsgrad}</p>
-          </div>
-        </Popup>
-      )}
+      <Ulykker></Ulykker>
     </Map>
   );
 }
