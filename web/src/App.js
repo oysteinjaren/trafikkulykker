c46fcabe9802ed5da46d1b26488810aaa82e23d1
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-08-13 14:14:52 +0200

    Enklere logikk for når vi må laste inn ulykker på nytt
    Bruker useLeafletBounds-hook fra pakken use-leaflet for å få tak i kartbildets grenser, og bruker disse som avhengigheter til useEffect som henter ulykker fra API.

--- web/src/App.js
+++ web/src/App.js
@@ -5,24 +5,13 @@ import "./App.css";
 import Ulykker from "./components/Ulykker";
 
 function App() {
-  const [måLasteData, setMåLasteData] = useState(true);
-
   return (
-    <Map
-      center={[63.430515, 10.395053]}
-      zoom={12}
-      onmoveend={() => {
-        setMåLasteData(true);
-      }}
-    >
+    <Map center={[63.430515, 10.395053]} zoom={12}>
       <TileLayer
         url="https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}"
         attribution="<a href='https://www.kartverket.no/'>Kartverket</a>"
       />
-      <Ulykker
-        måLasteData={måLasteData}
-        nullstillMåLasteData={() => setMåLasteData(false)}
-      ></Ulykker>
+      <Ulykker></Ulykker>
     </Map>
   );
 }
--- web/src/components/Ulykker/Ulykker.js
+++ web/src/components/Ulykker/Ulykker.js
@@ -1,50 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Marker, useLeaflet } from "react-leaflet";
+import { Marker } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
+import { useLeafletBounds } from "use-leaflet";
 import { Icon } from "leaflet";
 import axios from "axios";
 import UlykkePopup from "../UlykkePopup";
 
-function Ulykker(props) {
-  const { map } = useLeaflet();
-  const bounds = map.getBounds();
-
-  const [avgrensningsboks, setAvgrensningsboks] = useState({});
-
-  if (
-    bounds._southWest.lng !== avgrensningsboks.vest ||
-    bounds._southWest.lat !== avgrensningsboks.sør ||
-    bounds._northEast.lng !== avgrensningsboks.øst ||
-    bounds._northEast.lat !== avgrensningsboks.nord
-  ) {
-    setAvgrensningsboks({
-      vest: bounds._southWest.lng,
-      sør: bounds._southWest.lat,
-      øst: bounds._northEast.lng,
-      nord: bounds._northEast.lat,
-    });
-  }
-
-  const måLasteData = props.måLasteData;
-  const nullstillMåLasteData = props.nullstillMåLasteData;
+function Ulykker() {
+  const [[sør, vest], [nord, øst]] = useLeafletBounds();
 
   const [ulykker, setUlykker] = useState([]);
   const ulykkerUrl = "/api/ulykker";
+  const [aktivUlykke, setAktivUlykke] = React.useState(null);
+
   useEffect(() => {
     const hentUlykker = async () => {
       const response = await axios.get(ulykkerUrl, {
-        params: avgrensningsboks,
+        params: {
+          vest: vest,
+          sør: sør,
+          øst: øst,
+          nord: nord,
+        },
       });
       console.log(`Hentet ${response.data.length} ulykker`);
-      setUlykker(response.data);
+      setUlykker(response.data.filter((ulykke) => ulykke.koordinater != null));
     };
-    if (måLasteData) {
-      hentUlykker();
-      nullstillMåLasteData();
-    }
-  }, [avgrensningsboks, måLasteData, nullstillMåLasteData]);
-
-  const [aktivUlykke, setAktivUlykke] = React.useState(null);
+    hentUlykker();
+  }, [vest, sør, øst, nord]);
 
   function hentUlykkeIkon(alvorlighetsgrad) {
     var sti = "";
@@ -78,22 +61,23 @@ function Ulykker(props) {
   return (
     <MarkerClusterGroup>
       {ulykker &&
-        ulykker
-          .filter((ulykke) => ulykke.koordinater != null)
-          .map((ulykke) => (
-            <Marker
-              key={ulykke.id}
-              position={[ulykke.koordinater.lat, ulykke.koordinater.lon]}
-              icon={hentUlykkeIkon(ulykke.alvorlighetsgrad)}
-              onclick={() => {
-                setAktivUlykke(ulykke);
-              }}
-            />
-          ))}
+        ulykker.map((ulykke) => (
+          <Marker
+            key={ulykke.id}
+            position={[ulykke.koordinater.lat, ulykke.koordinater.lon]}
+            icon={hentUlykkeIkon(ulykke.alvorlighetsgrad)}
+            onclick={() => {
+              setAktivUlykke(ulykke);
+            }}
+          />
+        ))}
+
       {aktivUlykke && (
         <UlykkePopup
           aktivUlykke={aktivUlykke}
-          onClose={() => setAktivUlykke(null)}
+          onClose={() => {
+            setAktivUlykke(null);
+          }}
         ></UlykkePopup>
       )}
     </MarkerClusterGroup>
