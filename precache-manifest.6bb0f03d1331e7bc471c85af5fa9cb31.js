3c67aadf1df9b9c41431efd101d8a02313351d2b
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-08-14 03:53:47 +0200

    Updates

---
+++ precache-manifest.6bb0f03d1331e7bc471c85af5fa9cb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a7ea7ab2df89d5fbc35632172334ee",
+    "url": "/trafikkulyker/index.html"
+  },
+  {
+    "revision": "ce57adb949bbd8283152",
+    "url": "/trafikkulyker/static/css/main.3bac5fa9.chunk.css"
+  },
+  {
+    "revision": "b1e3164176e2177407d1",
+    "url": "/trafikkulyker/static/js/2.ddfec39b.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/trafikkulyker/static/js/2.ddfec39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce57adb949bbd8283152",
+    "url": "/trafikkulyker/static/js/main.149c15df.chunk.js"
+  },
+  {
+    "revision": "982ee9cb49959b456e4f",
+    "url": "/trafikkulyker/static/js/runtime-main.f068d7ba.js"
+  }
+]);
