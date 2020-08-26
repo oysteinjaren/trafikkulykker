5bdb933914d182a2b2c8c6e7b39caf1cdb2bce56
Øystein Grande Jaren <oystein.grande.jaren@bekk.no>
2020-08-26 14:47:46 +0200

    Dropper å skalere ned størrelsen på clustrene når man zoomer ut
    Dette viste seg å være vanskelig. Ville egentlig at enten målestokken på kartet eller antall ulykker i hele kartbilde skulle inngå i beregningen av hvor stor sirkel som måtte tegnes for clusteret. Men funksjonen man sender inn som iconCreateFunction til MarkerClusterGroup blir blir aldri oppdatert. Eller mer presist: Det virker som det leksikalske skopet til funksjonen blir bundet opp første gang Ulykker-komponenten rendres, slik at verdien av meterPerPiksel som funksjonen bruker blir låst til den initielle verdien. Når man så zoomer ut og inn senere, blir meterPerPiksel endret i Ulykker-komponenten, men ikke inni iconCreateFunction. Sannsynligvis relatert til denne issuen: https://github.com/yuzhva/react-leaflet-markercluster/issues/117

--- web/src/components/Ulykker/Ulykker.js
+++ web/src/components/Ulykker/Ulykker.js
@@ -9,13 +9,6 @@ import hentUlykker from "../../api/hentUlykker";
 
 function Ulykker() {
   const [[sør, vest], [nord, øst]] = useLeafletBounds();
-  const map = useLeafletMap();
-
-  const kartStørrelsePikslerY = map.getSize().y;
-  const meterPerBreddegrad = 111000;
-  const KartStørrelseMeterY = (nord - sør) * meterPerBreddegrad;
-  const meterPerPiksel = KartStørrelseMeterY / kartStørrelsePikslerY;
-  console.log(`meterPerPiksel er nå ${meterPerPiksel}`);
 
   const [ulykker, setUlykker] = useState([]);
   const [aktivUlykke, setAktivUlykke] = React.useState(null);
@@ -57,19 +50,12 @@ function Ulykker() {
   }
 
   function lagAntallsavhengigClusterIkon(cluster) {
-    console.log(`Lager cluster-ikon. meterPerPiksel er ${meterPerPiksel}`);
-
-    //const antallNoder = cluster.getChildCount();
-    const antallNoder = 10;
-    const ikonSkaleringsfaktor = 100000;
+    const antallNoder = cluster.getChildCount();
+    const ikonSkaleringsfaktor = 700;
 
     // Ikonets areal skal være proporsjonalt med antall noder i clusteret.
-    // Deler på meterPerPiksel for å justere ned størrelsen på clustrene når man zoomer ut
-    // (siden clustrene da inneholder mange flere noder, og ellers ville blitt veldig store).
-    // TODO: Inne i denne funksjonen har meterPerPiksel alltid sin initielle verdi. Den oppdateres ikke selv om meterPerPiksel utenfor funksjonen gjør det. Må finne ut hvorfor
     const ikonStørrelse = Math.max(
-      Math.sqrt((ikonSkaleringsfaktor * antallNoder) / Math.PI) /
-        meterPerPiksel,
+      Math.sqrt((ikonSkaleringsfaktor * antallNoder) / Math.PI),
       0 // Ikke la ikonet bli så lite at tallet som viser antall ikke får plass
     );
 
